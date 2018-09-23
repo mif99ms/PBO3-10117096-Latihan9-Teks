c225f67b6d3808d2365ef238bdbdf9641781da2b
mif99ms <43515183+mif99ms@users.noreply.github.com>
2018-09-23 18:43:15 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan9/teks/PBO310117096Latihan9Teks.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan9.teks;
+
+/**
+ *
+ * @author
+ * NAMA         : MIFTAH MUHAMMAD SIDQI
+ * NIM          : 10117096
+ * KELAS        : PBO3
+ * DESKRIPSI    : Program ini berisi program untuk menampilkan teks
+ */
+public class PBO310117096Latihan9Teks {
+
+    
+    public static void main(String[] args) {
+       char c;
+       char tab = '\t';
+       char newline = '\n';
+       c = 'A';
+       System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+       
+    }
+    
+}
